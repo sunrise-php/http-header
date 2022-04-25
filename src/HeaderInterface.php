ecad79fa616bda0b946de6fe28662e34fe468107
Анатолий Нехай <a.nehay@bothelp.io>
2022-04-25 06:47:56 +0500

    insignificant changes

--- src/HeaderInterface.php
+++ src/HeaderInterface.php
@@ -20,27 +20,27 @@ interface HeaderInterface
     /**
      * Regular Expression for a token validation
      *
-     * @link https://tools.ietf.org/html/rfc7230#section-3.2
-     *
      * @var string
+     *
+     * @link https://tools.ietf.org/html/rfc7230#section-3.2
      */
     public const RFC7230_TOKEN = '/^[\x21\x23-\x27\x2A\x2B\x2D\x2E\x30-\x39\x41-\x5A\x5E-\x7A\x7C\x7E]+$/';
 
     /**
      * Regular Expression for a field-value validation
      *
-     * @link https://tools.ietf.org/html/rfc7230#section-3.2
-     *
      * @var string
+     *
+     * @link https://tools.ietf.org/html/rfc7230#section-3.2
      */
     public const RFC7230_FIELD_VALUE = '/^[\x09\x20-\x7E\x80-\xFF]*$/';
 
     /**
      * Regular Expression for a quoted-string validation
      *
-     * @link https://tools.ietf.org/html/rfc7230#section-3.2
-     *
      * @var string
+     *
+     * @link https://tools.ietf.org/html/rfc7230#section-3.2
      */
     public const RFC7230_QUOTED_STRING = '/^[\x09\x20\x21\x23-\x5B\x5D-\x7E\x80-\xFF]*$/';
 
@@ -61,9 +61,9 @@ interface HeaderInterface
     /**
      * Converts the header to a string
      *
-     * @link http://php.net/manual/en/language.oop5.magic.php#object.tostring
-     *
      * @return string
+     *
+     * @link http://php.net/manual/en/language.oop5.magic.php#object.tostring
      */
     public function __toString();
 }
